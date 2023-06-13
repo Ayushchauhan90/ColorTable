58387a91fcb4ccaeaed5f310d140cbb9366e325f
Ayush Chauhan <aayuchauhan132@gmail.com>
2023-06-13 10:58:27 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+function changeColor(cell) {
+    var color = cell.innerText;
+    if (color) {
+        cell.style.backgroundColor = color;
+    } 
+     else  {
+        cell.style.backgroundColor = "";
+    }
+}
+
+function handleClick(event) {
+    var cell = event.target;
+    var previousCell = document.querySelector(".colored");
+
+    if (previousCell) {
+        previousCell.style.backgroundColor = "";
+        previousCell.classList.remove("colored");
+    }
+    changeColor(cell);
+    cell.classList.add("colored");
+}
+
+var cells = document.querySelectorAll("td");
+cells.forEach(function(cell) {
+    cell.addEventListener("click", handleClick);
+});
